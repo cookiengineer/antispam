7d4c0bf5b2602ed9b37c1d293afcafbfa145705f
Cookie Engineer <cookiengineer@protonmail.ch>
2024-11-13 15:03:30 +0100

    :bug: Fixes for email servers using random subdomains

--- source/structs/HostMap.go
+++ source/structs/HostMap.go
@@ -1,5 +1,7 @@
 package structs
 
+import "strings"
+
 type HostMap struct {
 	Hosts map[string]bool `json:"hosts"`
 }
@@ -22,10 +24,31 @@ func (hostmap *HostMap) SearchDomain(value string) bool {
 
 	var result bool = false
 
-	_, ok := hostmap.Hosts[value]
+	_, ok1 := hostmap.Hosts[value]
 
-	if ok == true {
+	if ok1 == true {
 		result = true
+	}
+
+	if result == false {
+
+		if strings.Contains(value, ".") {
+
+			tmp := strings.Split(value, ".")
+
+			if len(tmp) > 2 {
+				tmp = tmp[len(tmp)-2:]
+			}
+
+			domain := strings.Join(tmp, ".")
+			_, ok2 := hostmap.Hosts[domain]
+
+			if ok2 == true {
+				result = true
+			}
+
+		}
+
 	}
 
 	return result
